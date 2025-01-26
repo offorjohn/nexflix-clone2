955c878b1e98916171843e95886da33392c2e06b
offor <offorchukwuebuka3@gmail.com>
2025-01-26 23:07:19 +0100

    first commit

---
+++ src/config.js
@@ -0,0 +1,20 @@
+// replace below firebase config with your own
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAgC5ycZgLukJ8pFbbFa52YefeLRa7zlLw",
+  authDomain: "socials-clone-fe172.firebaseapp.com",
+  projectId: "socials-clone-fe172",
+  storageBucket: "socials-clone-fe172.firebasestorage.app",
+  messagingSenderId: "401509975886",
+  appId: "1:401509975886:web:94d45aef4255a96524001f",
+  measurementId: "G-QV7FGB2TMZ"
+};
+
+
+// craete a/c on themoviedb.org and replace access token below
+
+
+const TMDB_Access_Key="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYTE3NWVkZDE2M2MzNDQwZTQ5ZTYxZTk2YmRhZTE4ZCIsIm5iZiI6MTczNzI0ODYzMy45MDQsInN1YiI6IjY3OGM0Zjc5YWZjYjMyYzQ5OTQzMWE0NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mS1l2-wb-ZVVpCRq4a7lwUK4wDp-BqvoE4Te_01fAMw"
+  export {firebaseConfig, TMDB_Access_Key};
---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,54 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from 'react'
+import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
+import { TMDB_Access_Key } from '../../config'
+ 
+const Player = (props) => {
+  const state = props.location;
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    type: ""
+  })
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${TMDB_Access_Key}`
+    }
+  };
+
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results[0]))
+    .catch(err => console.error(err));
+    console.log(props);
+  },[id, options, props])
+  
+  
+
+  return (
+    <div className='player'>
+      <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}}/>
+      <iframe src={`https://www.youtube.com/embed/${apiData.key}`}
+      title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{state}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Player
